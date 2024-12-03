c120b973f1e4864d4a597a026fae434423ed63d1
Oleg.Semenov <Oleg.Semenov@x5.ru>
2024-12-03 21:53:31 +0300

    add home04

---
+++ homework_04/models.py
@@ -0,0 +1,47 @@
+"""
+создайте алхимичный engine
+добавьте declarative base (свяжите с engine)
+создайте объект Session
+добавьте модели User и Post, объявите поля:
+для модели User обязательными являются name, username, email
+для модели Post обязательными являются user_id, title, body
+создайте связи relationship между моделями: User.posts и Post.user
+"""
+
+import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, relationship
+
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    
+    posts = relationship("Post", back_populates="user")
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=False)
+    
+    user = relationship("User", back_populates="posts")
+
+
+engine = create_async_engine(PG_CONN_URI, echo=False)
+
+Session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
